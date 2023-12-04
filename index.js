9f0b9aa8c74091f8e2bbf9d2f8f3c4c166b20583
FlavioFMBorges <flyfmb@hotmail.com>
2023-12-04 16:24:19 -0300

    Ajustado o codigo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let saldoDeRankeadas = derrotaseVitorias(0, 50); //o primeiro parâmetro é derrota e depois vitória
+let saldoDeRankeadas = derrotaseVitorias(0, 101); //o primeiro parâmetro é derrota o segundo, vitória 
 let nivelJogador = nivel(saldoDeRankeadas)
 
 console.log("O Herói tem de saldo " + saldoDeRankeadas + " e está no nível de " + nivelJogador)
@@ -10,17 +10,16 @@ function derrotaseVitorias(derrotas, vitorias){
 
 function nivel(xpDoHeroi){
     let nivelDoHeroi = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"]
-    let ferro = 10
-    let bronze = [11, 20]
+    let Ferro = 10
+    let Bronze = [11, 20]
     let Prata = [21, 50]
     let Ouro = [51, 80]
     let Diamante = [81, 90]
     let Lendario = [91, 100]
-    let Imortal = [101]
 
-    if(xpDoHeroi <= ferro){
+    if(xpDoHeroi <= Ferro){
         x = nivelDoHeroi[0]
-    }else if((xpDoHeroi >= bronze[0]) && (xpDoHeroi <= bronze[1])){
+    }else if((xpDoHeroi >= Bronze[0]) && (xpDoHeroi <= Bronze[1])){
         x = nivelDoHeroi[1]
     }else if((xpDoHeroi >= Prata[0]) && (xpDoHeroi <= Prata[1])){
         x = nivelDoHeroi[2]
@@ -32,7 +31,7 @@ function nivel(xpDoHeroi){
         x = nivelDoHeroi[5]
     }else{
         x = nivelDoHeroi[6]
-        return x
-    }   
-}
+         }   
+         return x
+        }
 
